e6a70cd9036ca9c3b2cfce2767f1a5f0d76ff955
Amit Kumar Gupta <amitgupta542173@gmail.com>
2026-01-17 18:43:41 +0530

    main

--- src/popup/index.jsx
+++
@@ -1,7 +0,0 @@
-// src/popup/index.jsx
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Popup from './Popup';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Popup />);
